5352cf9e502de9f0f4c5bd996988fc990c732c41
MarikTar <100trk@gmail.com>
2020-01-18 20:55:06 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import parse from 'parse5';
+
+const url = 'https://www.olx.ua/otdam-darom/pol/';
+const proxyurl = 'https://cors-anywhere.herokuapp.com/';
+
+const asFetch = async () => {
+  const document = await fetch(url, {
+    headers: {
+      method: 'GET',
+      mode: 'cors',
+    },
+  });
+  try {
+    const data = await document.text();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+  // const document = parse();
+};
+
+
+
+asFetch();
