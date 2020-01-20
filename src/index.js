a5390e9ce60e064d88bb1ff0e1d9b5a07e7d0996
MarikTar <100trk@gmail.com>
2020-01-20 17:07:31 +0200

    add parse5

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
-import parse from 'parse5';
+import { parse } from 'parse5';
 
 const url = 'https://www.olx.ua/otdam-darom/pol/';
-const proxyurl = 'https://cors-anywhere.herokuapp.com/';
+const proxyurl = 'https://test-parser-trk.herokuapp.com/?href=';
 
 const asFetch = async () => {
-  const document = await fetch(url, {
-    headers: {
-      method: 'GET',
-      mode: 'cors',
-    },
-  });
   try {
-    const data = await document.text();
-    console.log(data);
+    const document = await fetch(proxyurl + url);
+    const textHtml = await document.text();
+    const data = parse(textHtml, {
+      scriptingEnabled: false,
+    });
+    console.log(data.childNodes[1]);
   } catch (error) {
     console.log(error);
   }
   // const document = parse();
 };
+
 asFetch();
